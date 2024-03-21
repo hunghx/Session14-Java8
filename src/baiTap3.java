56b8b8b0fe978273d637a1de8b4fffe340583762
hunghx <baby10051999@gmail.com>
2024-03-21 11:59:17 +0700

    update

--- src/baiTap3.java
+++ src/baiTap3.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class baiTap3 {
+import java.util.Arrays;
+import java.util.Random;
+
+public class baiTap3 {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Random().nextInt(1000);
+        }
+        int tong = Arrays.stream(arr).reduce(0,Integer::sum);
+        System.out.println(tong);
+    }
 }
